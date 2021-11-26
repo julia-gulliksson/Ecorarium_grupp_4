c9fe3f18aeea6f703022650b549cbf64b25f95d6
alexwesttorp <alexander.westtorp@yh.nackademin.se>
2021-11-26 19:34:49 +0100

    Ny scen med terräng & gameover

---
+++ Assets/Alex/Scripts/GameOverButtons.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverButtons : MonoBehaviour
+{
+    
+    public void PlayAgain() 
+    {
+        SceneManager.LoadScene("EcoTerrain 1");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Scen1 1");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
