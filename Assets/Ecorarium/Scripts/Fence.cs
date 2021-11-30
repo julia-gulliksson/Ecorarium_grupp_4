616f587567796cc9078c40445fe57d9c1b7e2323
NACKADEMIN\jugu2101 <Julia.gulliksson@yh.nackademin.se>
2021-11-30 13:14:49 +0100

    Get number closest to 0.5

--- Assets/Ecorarium/Scripts/Fence.cs
+++ Assets/Ecorarium/Scripts/Fence.cs
@@ -11,6 +11,7 @@ public class Fence : MonoBehaviour
     private List<Vector3> targetPoints = new List<Vector3>();
     [SerializeField] WolfSpawn wolfSpawn;
     List<float> lerpNumbers = new List<float>();
+    float lerpMiddle = 0.5f;
 
     void Start()
     {
@@ -18,34 +19,39 @@ public class Fence : MonoBehaviour
         fromPosition = transform.TransformPoint(meshFilter.mesh.bounds.max);
         toPosition = transform.TransformPoint(meshFilter.mesh.bounds.min);
         float lerpPoint = 0.1f;
-        float lerpBy = 0.8f / (wolfSpawn.nrOfWolves + 2);
-        Debug.Log(lerpBy + " LERP BY");
-        while (lerpNumbers.Count < (wolfSpawn.nrOfWolves + 2))
+        //TODO: Twerk these numbers
+        float lerpBy = 0.8f / (wolfSpawn.nrOfWolves);
+        while (lerpNumbers.Count < (wolfSpawn.nrOfWolves))
         {
             lerpNumbers.Add(lerpPoint);
             lerpPoint += lerpBy;
         }
-        var nearest = lerpNumbers.Aggregate((current, next) => Mathf.Abs((long)current - 0.5f) < Mathf.Abs((long)next - 0.5f) ? current : next);
+
+
         lerpNumbers.ForEach(l => Debug.Log(l));
+
         //float closest = ClosestToMiddle();
-        Debug.Log(nearest + " Nearest");
-        //StartCoroutine(CreateTargetPoints());
+        //Debug.Log(nearest + " Nearest");
+        StartCoroutine(CreateTargetPoints());
+    }
+
+    float ClosestToMiddle()
+    {
+        return lerpNumbers.Aggregate((min, next) => Mathf.Abs(next - lerpMiddle) > Mathf.Abs(min - lerpMiddle) ? min : next);
     }
 
     IEnumerator CreateTargetPoints()
     {
-        float lerpPoint = 0.1f;
-        float lerpBy = 0.8f / wolfSpawn.nrOfWolves;
         while (targetPoints.Count < wolfSpawn.nrOfWolves / 4)
         {
+            float lerpPoint = ClosestToMiddle();
             Vector3 targetPoint = Vector3.Lerp(fromPosition, toPosition, lerpPoint);
-            lerpPoint += lerpBy;
             targetPoints.Add(targetPoint);
+            lerpNumbers.Remove(lerpPoint);
             Instantiate(new GameObject("Point " + lerpPoint), targetPoint, Quaternion.identity);
 
             if (targetPoints.Count == wolfSpawn.nrOfWolves / 4)
             {
-                Debug.Log(targetPoints.Count + " All created");
                 // All target points are created
                 GameEventsManager.current.TargetPointsGenerated(targetPoints);
             }
@@ -54,23 +60,6 @@ public class Fence : MonoBehaviour
                 yield return null;
             }
         }
-    }
-
-    float ClosestToMiddle()
-    {
-        float closest = 0.5f;
-        foreach (float lerpNumber in lerpNumbers)
-        {
-            if (lerpNumber > 0 && lerpNumber <= Mathf.Abs(closest))
-            {
-                closest = lerpNumber;
-            }
-            else if (lerpNumber < 0 && -lerpNumber < Mathf.Abs(closest))
-            {
-                closest = lerpNumber;
-            }
-        }
-        return closest;
     }
 
     //TODO: Health system
