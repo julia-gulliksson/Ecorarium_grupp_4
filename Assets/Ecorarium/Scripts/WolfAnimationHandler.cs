568ffd4637888936d514a49dc72b6c56506eae68
RSPHenrik <rickard.pettersten@yh.nackademin.se>
2021-12-02 15:04:48 +0100

    Implemetation of Attack Animation

--- Assets/Ecorarium/Scripts/WolfAnimationHandler.cs
+++ Assets/Ecorarium/Scripts/WolfAnimationHandler.cs
@@ -14,27 +14,21 @@ public class WolfAnimationHandler : MonoBehaviour
     
     void Start()
     {
-        GameEventsManager.current.onWolfCollide += Attack;
+        
         wolfAnimation = GetComponent<Animator>();
         wolfNa = GetComponent<NavMeshAgent>();
         wolfController = GetComponent<Wolf>();
         
     }
 
-    private void Attack(bool obj, int id)
-    {
-        if(id == wolfController.id)
-        {
-            wolfAnimation.SetBool("Attack", obj);
-            print("Attack " + id);
-        }
-        
-    }
+   
 
     // Update is called once per frame
     void Update()
     {
         wolfAnimation.SetFloat("Velocity", GetNormalVelocity());
+
+        if(wolfController.hasFoundTarget) wolfAnimation.SetBool("Attack", true);
     }
 
     private float GetNormalVelocity()
