ebc2576872e43c17d5e91a5ac836b9ef014dbe49
alexwesttorp <alexander.westtorp@yh.nackademin.se>
2021-12-06 11:36:19 +0100

    Teleport inte klar

---
+++ Assets/Alex/Scripts/TeleportRay.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TeleportRay : MonoBehaviour
+{
+
+    public XRController leftTeleportRay;
+    public InputHelpers.Button teleportActivationButton;
+    public float activationThreshold = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(leftTeleportRay)
+        {
+            leftTeleportRay.gameObject.SetActive(CheckIfActivated(leftTeleportRay));
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationThreshold);
+        return isActivated;
+    }
+}
