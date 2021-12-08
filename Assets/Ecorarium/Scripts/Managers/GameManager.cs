3206433ca7cc73f7966e5d28a3027ae73257eb11
julia-gulliksson <Julia.Gulliksson@yh.nackademin.se>
2021-12-08 18:09:56 +0100

    Display nr of days survived in game over UI

--- Assets/Ecorarium/Scripts/Managers/GameManager.cs
+++ Assets/Ecorarium/Scripts/Managers/GameManager.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private void OnEnable()
     {
-        
+        GameEventsManager.current.onGameOver += GameOver;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        GameEventsManager.current.onGameOver -= GameOver;
+    }
+
+    private void GameOver()
+    {
+        // Show game over ui, lock player position
+    }
+
+    public void RestartGame()
+    {
+        //Restart
+    }
+
+    public void MainMenu()
+    {
+        // Go to main menu
     }
 }
