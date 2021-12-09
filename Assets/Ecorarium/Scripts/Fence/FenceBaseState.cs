08bbc44e9aff70fdd5f884da3014710e0d9def95
RSPHenrik <rickard.pettersten@yh.nackademin.se>
2021-12-09 12:48:24 +0100

    Rename and add nameSpace
    TheLoneHerder

--- Assets/Ecorarium/Scripts/Fence/FenceBaseState.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class FenceBaseState
-{
-    public abstract void EnterState(FenceStateManager fence);
-    public abstract void ExitState();
-}
---
+++ Assets/TheLoneHerder/Scripts/Fence/FenceRepairState.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+
+namespace TheLoneHerder
+{
+    public class FenceRepairState : FenceBaseState
+    {
+        FenceStateManager fence;
+        float repairSpeed = 0.1f;
+        int savedHealth;
+        bool isRepairing = false;
+
+        public override void EnterState(FenceStateManager fenceRef)
+        {
+            fence = fenceRef;
+            savedHealth = fence.Health;
+            if (!fence.MaxHealthReached() && !isRepairing)
+            {
+                fence.repairHealth = fence.StartCoroutine(Repair());
+            }
+        }
+
+        public override void ExitState()
+        {
+            if (isRepairing)
+            {
+                fence.StopCoroutine(fence.repairHealth);
+                isRepairing = false;
+            }
+        }
+
+        IEnumerator Repair()
+        {
+            isRepairing = true;
+            while (savedHealth < fence.MaxHealth)
+            {
+                savedHealth++;
+                if (fence.side == 1)
+                {
+                    Debug.Log("Repairing! " + savedHealth);
+
+                }
+                fence.SendUpdatedHealth(savedHealth);
+                fence.UpdateHealth(savedHealth);
+                yield return new WaitForSeconds(repairSpeed);
+            }
+            fence.SwitchState(fence.DamageState);
+        }
+    }
+}
