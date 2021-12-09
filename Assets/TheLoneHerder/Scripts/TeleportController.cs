3d1514dee887e6ceb8f15c50a1721d67a5237763
NACKADEMIN\jugu2101 <Julia.gulliksson@yh.nackademin.se>
2021-12-09 14:12:22 +0100

    Fix teleport on big rock and bridges, rename Scen1 to Lobby

--- Assets/TheLoneHerder/Scripts/TeleportController.cs
+++ Assets/TheLoneHerder/Scripts/TeleportController.cs
@@ -4,8 +4,6 @@ using UnityEngine.Events;
 
 public class TeleportController : MonoBehaviour
 {
-
-    //public GameObject baseControllerGameObject;
     public GameObject teleportationGameObject;
 
     public InputActionReference teleportActivationReference;
@@ -13,11 +11,16 @@ public class TeleportController : MonoBehaviour
     public UnityEvent onTeleportActivate;
     public UnityEvent onTeleportCancel;
 
-    private void Start()
+    private void OnEnable()
     {
         teleportActivationReference.action.performed += TeleportModeActivate;
         teleportActivationReference.action.canceled += TeleportModeCancel;
+    }
 
+    private void OnDisable()
+    {
+        teleportActivationReference.action.performed -= TeleportModeActivate;
+        teleportActivationReference.action.canceled -= TeleportModeCancel;
     }
 
     private void TeleportModeCancel(InputAction.CallbackContext obj) => Invoke("DeactivateTeleporter", .1f);
@@ -25,6 +28,6 @@ public class TeleportController : MonoBehaviour
     void DeactivateTeleporter() => onTeleportCancel.Invoke();
 
     private void TeleportModeActivate(InputAction.CallbackContext obj) => onTeleportActivate.Invoke();
-    
+
 
 }
